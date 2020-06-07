cb654607ab22f6eb6893e9b55b04db520fd24fc4
anmolgupta <anmolgupta1311@gmail.com>
2020-06-07 22:34:49 +0530

    Update Pipfile.lock and add User class

--- back-end/app.py
+++ back-end/app.py
@@ -10,5 +10,15 @@ db = SQLAlchemy(app)
 class Test1(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	roll_no = db.Column(db.Integer, unique = True, nullable = False)
+	name = db.Column(db.String(20), nullable = False)
+	email_id = db.Column(db.String(20), nullable = False)
+	password = db.Column(db.String(60), nullable = False)
+
+	def __repr__(self):
+	 return f"User({self.id} + {self.roll_no} + {self.name} + {self.email_id} + {self.password})"
+
 if __name__ == '__main__':
 	app.run()
