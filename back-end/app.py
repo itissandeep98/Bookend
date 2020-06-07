d498cc38e3d365dbf8c73c9854f3163d4e114f00
anmolgupta <anmolgupta1311@gmail.com>
2020-06-07 23:31:14 +0530

    Modify User class

--- back-end/app.py
+++ back-end/app.py
@@ -13,12 +13,12 @@ class Test1(db.Model):
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	roll_no = db.Column(db.Integer, unique = True, nullable = False)
-	name = db.Column(db.String(20), nullable = False)
-	email_id = db.Column(db.String(20), nullable = False)
+	name = db.Column(db.String(254), nullable = False)
+	email_id = db.Column(db.String(254), nullable = False)
 	password = db.Column(db.String(60), nullable = False)
 
 	def __repr__(self):
-	 return f"User({self.id} + {self.roll_no} + {self.name} + {self.email_id} + {self.password})"
+	 return f"User('{self.id}', '{self.roll_no}', '{self.name}', '{self.email_id}', '{self.password}')"
 
 if __name__ == '__main__':
 	app.run()
