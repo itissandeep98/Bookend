070a66a457432a62e36f6a583e89390966379326
Sandeep <itissandeep98@gmail.com>
2020-06-17 20:43:10 +0530

    ui update

--- front-end/src/Components/Home/SearchResults.js
+++ front-end/src/Components/Home/SearchResults.js
@@ -11,22 +11,22 @@ export default class SearchResults extends Component {
 					<tr key={ad.id}>
 						<td>{ad.book_name}</td>
 						<td>{ad.author}</td>
-						<td>{ad.description}</td>
+						<td className="col-3">{ad.description}</td>
 						<td>{ad.transaction_type}</td>
-						<td>{ad.price ? ad.price : ""}</td>
-						<td><Button outline onClick={() => { this.props.handleInfo(ad.user_id) }}><span className="fa fa-info-circle"></span></Button ></td>
+						<td className="col-2">{ad.price ? ad.price : ""}</td>
+						<td ><Button outline onClick={() => { this.props.handleInfo(ad.user_id) }}><span className="fa fa-info-circle"/></Button ></td>
 					</tr>
 				)
 			})
 			return <Table striped bordered hover responsive>
 							<thead>
 								<tr>
-									<th>Title</th>
-									<th>Author</th>
-									<th>Description</th>
+									<th>Title <span className="fa fa-book" /></th>
+									<th>Author <span className="fa fa-user"/></th>
+									<th>Description <span className="fa fa-file"/></th>
 									<th>Type</th>
-									<th>Price</th>
-									<th>Seller</th>
+									<th>Price <span className="fa fa-rupee"/></th>
+									<th>Seller <span className="fa fa-tags" /></th>
 								</tr>
 							</thead>
 							<tbody>
@@ -35,7 +35,7 @@ export default class SearchResults extends Component {
 						</Table>
 		}
 		else{
-			return <h2> No Results Found<span className="fa fa-filter"></span></h2>
+			return <h2> No Books Here<span className="fa fa-filter"/></h2>
 		}
 	}
 }
