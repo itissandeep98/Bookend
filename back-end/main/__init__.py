6ff8277ba8a973ee0c6edf3d9a6ddfb1a5191588
Sandeep <itissandeep98@gmail.com>
2020-07-08 14:56:24 +0530

    frontend branch initialize

--- back-end/main/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.secret_key = '5ZN5zi!45QUsGG'
-app.config.from_pyfile('config.cfg')
-db = SQLAlchemy(app)
-
-from main import routes
