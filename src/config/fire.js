6346c91101771515ca99b087eac10bb6617fbdbe
sandeep <itissandeep98@gmail.com>
2020-07-28 23:45:27 +0530

    bug fix

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 var firebaseConfig = {
-	apiKey: process.env.apiKey,
-	authDomain: process.env.authDomain,
-	databaseURL: process.env.databaseURL,
-	projectId: process.env.projectId,
-	storageBucket: process.env.storageBucket,
-	messagingSenderId: process.env.messagingSenderId,
-	appId: process.env.appId,
-	measurementId: process.env.measurementId
+	apiKey: "AIzaSyBUyX7qW4GP6G9imuLAaUQAj4Ax0U6t_wE", 
+	authDomain: "bookend-35b35.firebaseapp.com", 
+	databaseURL: "https://bookend-35b35.firebaseio.com", 
+	projectId: "bookend-35b35", 
+	storageBucket: "bookend-35b35.appspot.com", 
+	messagingSenderId: "494667459632", 
+	appId: "1:494667459632:web:0c1066fbe356d81e6011f5", 
+	measurementId: "G-WEN2EVKDTD"
 };
 
 const fire=firebase.initializeApp(firebaseConfig);
